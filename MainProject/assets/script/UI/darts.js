475de26c077f71f1a92c7aa5cc1304793bf2d603
psychokiller <fucked@psychokiller.me>
2018-11-07 15:22:35 +0800

    准星呼吸动作

--- MainProject/assets/script/UI/darts.js
+++ MainProject/assets/script/UI/darts.js
@@ -15,9 +15,17 @@ cc.Class({
         dartPanel: cc.Node,
         dartSight: cc.Node,
         sightStar: [cc.Node, cc.Node, cc.Node, cc.Node],
-        _Movespeed: {
+        _moveSpeed: {
             displayName: '准星跟随速度',
             default: .4
+        },
+        _sightStarSpeed: {
+            displayName: '准星前后呼吸值',
+            default: {}
+        },
+        _sightStarValue: {
+            displayName: '准星前后跳动数值',
+            default: []
         }
         // foo: {
         //     // ATTRIBUTES:
@@ -46,7 +54,6 @@ cc.Class({
 
         // 瞄准器移动
         this.node.on(cc.Node.EventType.TOUCH_MOVE, (event) => {
-            // console.log(event.getLocation())
             this._sightMove(event.getDelta())
         })
 
@@ -65,25 +72,30 @@ cc.Class({
     _sightInit: function (position) {
         this.dartSight.active = true
         this.dartSight.setPosition(position)
+        // 镜头拉近动作
+
         // 前后前后动作
         this._sigthAimat()
         // 8个方向晃动动作
         this._sightActionBuff()
     },
     
-    // 准星前后前后移动
+    // 准星前后移动模拟呼吸动作
     _sigthAimat: function () {
-        let topAction = cc.moveBy(.5, cc.v2(0, 20))
-        this.sightStar[0].runAction(topAction)
-
-        let rigthAction = cc.moveBy(.5, cc.v2(20, 0))
-        this.sightStar[1].runAction(rigthAction)
-
-        let bottomAction = cc.moveBy(.5, cc.v2(0, -20))
-        this.sightStar[2].runAction(bottomAction)
-
-        let leftAction = cc.moveBy(.5, cc.v2(-20, 0))
-        this.sightStar[3].runAction(leftAction)
+        for(let key in this._sightStarValue) {
+            this.sightStar[key].runAction(cc.sequence(
+                cc.moveBy(this._sightStarSpeed.first, this._sightStarValue[key].value, 0),
+                cc.moveBy(this._sightStarSpeed.first, this._sightStarValue[key].value, 0).reverse(),
+                cc.callFunc(() => {
+                    this.sightStar[key].runAction(cc.repeatForever(
+                        cc.sequence(
+                            cc.moveBy(this._sightStarSpeed.value, this._sightStarValue[key].value, 0),
+                            cc.moveBy(this._sightStarSpeed.value, this._sightStarValue[key].value, 0).reverse(),
+                        )
+                    ))
+                }, this.sightStar[key])
+            ))
+        }
     },
     
     _sightRollDirection: function (dt) {
@@ -102,20 +114,44 @@ cc.Class({
     // 瞄准器移动
     _sightMove: function (delta) {
         // 跟随
-        this.dartSight.runAction(cc.moveBy(this._Movespeed, delta))
+        this.dartSight.runAction(cc.moveBy(this._moveSpeed, delta))
     },
 
     // 销毁瞄准器 & 发射 & 复位
     _sightDestroy: function () {
         this.dartSight.active = false
-        // 准幸复位
-        this.sightStar[0].setPosition(cc.v2(0, 0))
-        this.sightStar[1].setPosition(cc.v2(20, -20))
-        this.sightStar[2].setPosition(cc.v2(0, -40))
-        this.sightStar[3].setPosition(cc.v2(-20, -20))
+        // 准星复位
+        for (let key in this._sightStarValue) {
+            this.sightStar[key].setPosition(this._sightStarValue[key].reset)
+            cc.director.getActionManager().removeAllActionsFromTarget(this.sightStar[key])
+        }
     },
 
     onLoad: function () {
+        // 准星前后呼吸值
+        this._sightStarSpeed = {
+            first: .5,  // 第一次呼吸速度
+            value: .4   // 之后循环呼吸速度
+        }
+        // 准星前后跳动数值
+        this._sightStarValue = [{
+            name: 'top',
+            reset: cc.v2(0, 0),
+            value: cc.v2(0, 40)
+        },{
+            name: 'right',
+            reset: cc.v2(20, -20),
+            value: cc.v2(40, 0)
+        },{
+            name: 'bottom',
+            reset: cc.v2(0, -40),
+            value: cc.v2(0, -40)
+        },{
+            name: 'left',
+            reset: cc.v2(-20, -20),
+            value: cc.v2(-40, 0)
+        }]
+
         // 瞄准器默认关闭
         this._sightDestroy()
     },
